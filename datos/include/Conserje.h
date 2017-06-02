00bae9f584446c96b827770ae7a642aacb09da06
alexa1999 <anel.veliz@ucsp.edu.pe>
2017-06-02 12:21:45 -0500

    Update Conserje.h

--- datos/include/Conserje.h
+++ datos/include/Conserje.h
@@ -11,6 +11,6 @@ class Conserje:public Persona{
     int sotano=7;
 public:
     Conserje(string n,Birthay b,int d,string p):Persona(n,b,d,p){}
-    void mostrar(){cout<<name<<" "<<sotano<<" "<<cumple.edad()<<" "<<dni<<" "<<pais;}
+    void mostrar(){cout<<" nombre:  "<<name<<" numero de sotano:  "<<sotano<<"  edad: "<<cumple.edad()<<"  dni:  "<<dni<<"  pais :"<<pais;}
 };
 #endif // CONSERJE_H
